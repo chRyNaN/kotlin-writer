cbb59a723f62eeae6f8f0ad1b4824028dd10bc2f
Chris <ckeenan@starry.com>
2018-10-31 12:33:36 -0400

    Added KotlinCodeBlockBuilder Functions

--- src/main/kotlin/com/chrynan/kotlinwriter/KotlinCodeBlockBuilder.kt
+++ src/main/kotlin/com/chrynan/kotlinwriter/KotlinCodeBlockBuilder.kt
@@ -6,7 +6,33 @@ import com.squareup.kotlinpoet.CodeBlock
 
 class KotlinCodeBlockBuilder(private val builder: CodeBlock.Builder) {
 
-    // TODO add DSL functions for CodeBlock.Builder
+    fun named(format: String, args: Map<String, *>) = builder.addNamed(format, args)
+
+    fun named(args: Map<String, *>, block: () -> String) = builder.addNamed(block(), args)
+
+    fun code(format: String, vararg args: Any?) = builder.add(format, args)
+
+    fun code(vararg args: Any?, block: () -> String) = builder.add(block(), args)
+
+    fun beginControlFlow(controlFlow: String, vararg args: Any?) = builder.beginControlFlow(controlFlow, args)
+
+    fun beginControlFlow(vararg args: Any?, block: () -> String) = builder.beginControlFlow(block(), args)
+
+    fun nextControlFlow(controlFlow: String, vararg args: Any?) = builder.nextControlFlow(controlFlow, args)
+
+    fun nextControlFlow(vararg args: Any?, block: () -> String) = builder.nextControlFlow(block(), args)
+
+    fun endControlFlow() = builder.endControlFlow()
+
+    fun statement(format: String, vararg args: Any?) = builder.addStatement(format, args)
+
+    fun statement(vararg args: Any?, block: () -> String) = builder.addStatement(block(), args)
+
+    fun codeBlock(codeBlock: CodeBlock) = builder.add(codeBlock)
+
+    fun indent() = builder.indent()
+
+    fun unindent() = builder.unindent()
 
     fun build() = builder.build()
 }
